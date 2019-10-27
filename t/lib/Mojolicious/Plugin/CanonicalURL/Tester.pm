755907121e4457fae485bc278526442415bdd51d
Adam Hopkins <violapiratejunky@gmail.com>
2019-10-27 18:46:18 -0500

    fix 404 broken test

--- t/lib/Mojolicious/Plugin/CanonicalURL/Tester.pm
+++ t/lib/Mojolicious/Plugin/CanonicalURL/Tester.pm
@@ -8,7 +8,6 @@ use lib path(__FILE__)->dirname->to_string;
 use Mojolicious::Plugin::CanonicalURL::Tester::Captures;
 use Mojolicious::Plugin::CanonicalURL::Tester::EndWithSlash;
 use Mojolicious::Plugin::CanonicalURL::Tester::InlineCode;
-use Mojolicious::Plugin::CanonicalURL::Tester::NotFoundNotCanonicalized;
 use Mojolicious::Plugin::CanonicalURL::Tester::OnlyPathChanges;
 use Mojolicious::Plugin::CanonicalURL::Tester::RemoveTrailingSlashes;
 use Mojolicious::Plugin::CanonicalURL::Tester::ShouldAndShouldNotCanonicalizeRequestTogether;
@@ -45,7 +44,6 @@ sub _test_with_args {
     Mojolicious::Plugin::CanonicalURL::Tester::Captures->new->test(@args);
     Mojolicious::Plugin::CanonicalURL::Tester::EndWithSlash->new->test(@args);
     Mojolicious::Plugin::CanonicalURL::Tester::InlineCode->new->test(@args);
-    Mojolicious::Plugin::CanonicalURL::Tester::NotFoundNotCanonicalized->new->test(@args);
     Mojolicious::Plugin::CanonicalURL::Tester::OnlyPathChanges->new->test(@args);
     Mojolicious::Plugin::CanonicalURL::Tester::RemoveTrailingSlashes->new->test(@args);
     Mojolicious::Plugin::CanonicalURL::Tester::ShouldCanonicalizeRequest->new->test(@args);
